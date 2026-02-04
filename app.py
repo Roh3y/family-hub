fc8f3a1ff20398f589b91bac57aafa8064db745f
Roh3y <doctor46rohan@outlook.com>
2026-02-04 19:46:11 +1100

    Update app.py
    Title change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import date
 
 # 1. Page Setup
 st.set_page_config(page_title="Family Hub", layout="wide")
-st.title("😍 Family Life Hub 😍")
+st.title("😍 Family Hub 😍")
 
 # 2. Connection Setup
 try:
@@ -168,4 +168,5 @@ elif page == "Water Tests":
     if not df_water.empty:
         t_filter = st.radio("View Tank:", ["154L", "20L"], horizontal=True)
         st.dataframe(df_water[df_water["Tank"] == t_filter].sort_values("Date", ascending=False), use_container_width=True, hide_index=True)
+
 
